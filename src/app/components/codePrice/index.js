eb68903dbcc8aacf31bb942945a9a69c330dc177
koudid <koudid@azzedin29>
2024-02-22 17:16:58 +0100

    ????caml rahoum en commentaire

--- src/app/components/codePrice/index.js
+++ src/app/components/codePrice/index.js
@@ -1,35 +1,45 @@
 'use client'
 import "../../styles/codePrice.css"
 import Form from 'react-bootstrap/Form';
-import { useEffect, useRef,useState } from "react";
+import { Modal, Button } from 'react-bootstrap';
+import { createContext, useEffect, useRef,useState, } from "react";
 import axios from 'axios';
+
+//utilsier create context pour peut transporter larticel vers la page des tableaux
+export default ArtcileConetxt=createContext()
 
 export default function CodePrice(){
    const cbRef = useRef(null);
    const qteRef = useRef(null);
    const prixRef = useRef(null);
    const [input,setInput]=useState({})
-   const [ajouter,setAjouter]=useState(false);
+   /*const [prix,setPrix]=useState(0)
+   const [id,setPrix]=useState(0)
+    const [ajouter,setAjouter]=useState(false);
+   const [showModal, setShowModal] = useState(true);
+   const handleClose = () => setShowModal(false); 
+   const [nvInputs,setNvInpust]=useState({}); 
+   const [clickedArticle,SetClickedArticle]=useState('')*/
 
 
 
-const [famille, setFamille] = useState('');
-const [article, setArticl] = useState('');
-const [id, setId] = useState('');
+/* const [famille, setFamille] = useState([]);
+const [article, setArticl] = useState([]);
+
 
 useEffect(() => {
   Promise.all([
     axios.get('/api/famille'),
     axios.get('/api/articl'),
-    axios.get('/api/id')
+    
   ])
   .then(([familleRes, articlRes, idRes]) => {
     setFamille(familleRes.data);
     setArticl(articlRes.data);
-    setId(idRes.data);
+  
   })
   .catch(err => console.log(err));
-}, []);
+}, []); */
 
 
 
@@ -50,12 +60,13 @@ useEffect(() => {
      const handleKeyPress = (event) => {
       if (event.key === 'Enter') {
         if (input.cb.length === 13) {
-          axios.post('/api/codebar',{codebar=input.cd})
+          axios.post('/api/codebar',{codebar=input.cb})
           .then(res=>{
             if(res.data.message==="Success"){
                qteRef.current.focus();
             }
             else{
+               alert('Element non Trouver')
                setAjoute(true);
             }
           })
@@ -74,8 +85,7 @@ useEffect(() => {
       if (!isNaN(value)) {
         // Limiter la longueur à 13 chiffres pour le champ 'cb'
         if (name === 'cb' && value.length >= 13) {
-          e.preventDefault(); 
-          
+          return;
         }
         
         setInput(val => ({ ...val, [name]: value }));
@@ -83,6 +93,37 @@ useEffect(() => {
         setInput(val => ({ ...val, [name]: '' }));
       }
     };
+
+     /* const HandelNvInput = (e) => {
+      const name = e.target.name;
+      let value = e.target.value;
+        setInput(val => ({ ...val, [name]: value }));
+     
+    }
+
+    const HandelAjouterElement=(e)=>{
+      e.preventDefault()
+      axios.post('/api/AjouterElement',{article:nvInputs.article,prix:nvInputs.prix,chemin:nvInputs.chemin,codebare:input.cb})
+      .then(res=>{
+        alert('Vous Ave Ajouter Un element')
+        console.log(res)})
+      .catch(err=>console.log(err))
+    } */
+
+  /*   const AfficherPrixEtId=(e)=>{
+     e.preventDefault();
+     axios.get('/api/prixArticle')
+     .then(res=>{
+      if(res.data.message==='success'){
+        setPrix(res.data.prix)
+        setId(res.data.id)
+        SetClickedArticle(e.target.value)
+      }else{
+        console.log('Prix non etablit')
+      }
+     })
+     .catch(err=>console.log(err));
+    } */
 
     return(
         <>
@@ -104,7 +145,7 @@ useEffect(() => {
 
                </Form.Select>
                <p style={{marginRight:"26px"}}>ID</p>
-                 <input className="idInput"></input>
+                 <input className="idInput" name="id" /* value={id} */></input>
                  <Form.Select className="familleForm">  
 
                {/* {id ? id.map((item,index)=>{
@@ -116,27 +157,62 @@ useEffect(() => {
                   <div className="row3">
                   <p style={{marginRight:"28px"}}>Article :</p>
                   <Form.Select className="familleForm">   
-               
+                   
                {/* {article ? article.map((item,index)=>{
-                      <option key={index}>{article.id}</option>
+                      <option key={index} onClick={AfficherPrixEtId}>{article.name}</option>
                }):<h4>Loading.....</h4>} */}
 
                </Form.Select>
                <p style={{marginRight:"5px"}}>Prix :</p>
-               <input className="priceInput" ref={prixRef} name='prix' onChange={HandelInput} value={input.prix}></input>
+               <input className="priceInput" ref={prixRef} name='prix' onChange={HandelInput} /* value={prix} */></input>
                   </div>
                </div>
                <div className="priceDiv">
                 <p>0,00</p>
                </div>
-               <div>
+
+                {/* <div>
                {ajouter ? (
-  <div>
-    {/* afficher le div dajouter un element dans les article */}
-  </div>
-) : ''}
-               </div>
-               
+                         <div>
+                      
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Ajouter Article</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="ajout-input">
+          <label>Code Bare:
+          <input type="text" readOnly value={input.cb} />
+          </label>
+         <label>Article:
+         <input type="text" name="article" className="article" value={nvInputs.article} onChange={HandelNvInput} />
+         </label>
+         <label>Prix:
+         <input type="text" name="prix" className="prix" value={nvInputs.prix} onChange={HandelNvInput}  />
+         </label>
+         <label>Chemin:
+         <input type="text" name="chemin" className="chemin" value={nvInputs.chemin} onChange={HandelNvInput}  />
+         </label>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={HandelAjouterElement}  >
+          Ajouter
+          </Button>
+          <Button variant="secondary" >
+          Imp
+          </Button>
+          <Button variant="secondary" >
+          Toujour Ajouter
+          </Button>
+        </Modal.Footer>
+      </Modal>
+                         </div>
+                          ) : ''}
+               </div>  */}
+
+    <ArtcileConetxt.Provider value={input.id,clickedArticle,}>
+      
+    </ArtcileConetxt.Provider>
             </div>
         </>
     )
