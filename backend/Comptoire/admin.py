be485a206fc5315c76be352dcfdb2651608340fa
Darkyy69 <93149681+Darkyy69@users.noreply.github.com>
2024-03-04 15:59:17 +0000

    Delete backend directory

--- backend/Comptoire/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Article, Famille, S_famille, S_article
-# Register your models here.
-
-admin.site.register(Article)
-admin.site.register(Famille)
-admin.site.register(S_famille)
-admin.site.register(S_article)
--- backend/Comptoire/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import comptoire_view, import_articles_from_txt
-
-urlpatterns = [
-    # path('api/', ), 
-    path('', comptoire_view, name='comptoire'),
-    path('abc/', import_articles_from_txt, name='ssss'),
-]
